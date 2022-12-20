7e49013079e6a63f537538b9222eebed966b77de
fenya <dim74.09@mail.ru>
2022-12-21 01:20:55 +0500

    help added

--- IEEE754.mjs
+++ IEEE754.mjs
@@ -1,4 +1,33 @@
 import Float from "./Float.mjs";
 
-let test = new Float(process.argv[2]);
-
+switch (process.argv[2])
+{
+    case 'convert':
+    {
+        let float = new Float(process.argv[3], 8, 32);
+        console.log(float.standartForm);
+        break;
+    }
+    case 'add':
+    {
+        let float1 = new Float(process.argv[3], 8, 32);
+        let float2 = new Float(process.argv[4], 8, 32);
+        let float3 = Float.add(float1, float2);
+        console.log(float3.standartForm);
+        console.log(float3.normalForm);
+        break;
+    }
+    case '--help':
+    case '-h':
+    case '/?':
+    {
+        console.log("Usage: node IEEE754 convert 'float'");
+        console.log("OR: node IEEE754 add 'float1' 'float2'\n (addition only for same signs yet)");
+        break;
+    }
+    default:
+    {
+        console.log("Type error. Type node IEEE754 --help for info");
+        break;
+    }
+}
